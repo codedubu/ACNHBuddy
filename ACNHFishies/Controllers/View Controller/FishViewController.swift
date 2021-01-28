ff0c98a204b30e0959ec5fae7e3bb531dd1b5c94
codedubu <mccaine.river@gmail.com>
2021-01-28 15:59:00 -0800

    Minor graphical changes with price formatting to have commas.

--- ACNHFishies/Controllers/View Controller/FishViewController.swift
+++ ACNHFishies/Controllers/View Controller/FishViewController.swift
@@ -17,14 +17,22 @@ class FishViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var rarityLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
-    
     @IBOutlet weak var chatBubbleImageImageView: UIImageView!
+    @IBOutlet weak var backgroundDirtImageView: UIImageView!
+    @IBOutlet weak var goFishingButton: UIButton!
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         fishSearchBar.delegate = self
         hideKeyboardWhenTappedAround()
+        
+        backgroundDirtImageView.layer.masksToBounds = true
+        backgroundDirtImageView.layer.cornerRadius = 16.8
+        backgroundDirtImageView.layer.cornerCurve = .continuous
+        goFishingButton.layer.cornerRadius = 15.8
+        goFishingButton.layer.cornerCurve = .continuous
+        
     }
     
     // MARK: - Actions
@@ -40,9 +48,9 @@ class FishViewController: UIViewController {
                     print(error)
                 }
             }
-            
         }
         chatBubbleImageImageView.image = UIImage(named: "chatbubble")
+        
     }
     
     // MARK: - Helper Functions
@@ -57,7 +65,7 @@ class FishViewController: UIViewController {
                     self.catchPhraseLabel.text = "\(fish.catchPhrase)"
                     self.locationLabel.text = " Location: \n \(fish.availability.location)"
                     self.rarityLabel.text = " Rarity: \n \(fish.availability.rarity)"
-                    self.priceLabel.text = " Price: \(fish.price) bells"
+                    self.priceLabel.text = " Price: \(fish.price.withCommas()) bells"
                     
                     
                 case .failure(let error):
@@ -86,5 +94,13 @@ extension FishViewController: UISearchBarDelegate {
                 }
             }
         }
+    }
+}
+
+extension Int {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
     }
 }
